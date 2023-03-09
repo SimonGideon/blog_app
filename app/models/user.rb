1b6af46f617d830b770bd8c0dcd8309bc98c5941
SimonGideon <simongideon918@gmail.com>
2023-03-10 00:30:45 +0300

    Add test

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,8 @@ class User < ApplicationRecord
   has_many :posts
   has_many :comments
   has_many :likes
-  validate :name, presence: true
-  validate :posts_counter, numericality: { only_integer: true }
+  validates :name, presence: true
+  validates :posts_counter, numericality: { only_integer: true }
 
   def recent_posts
     posts.order(created_at: :desc).limit(3)
