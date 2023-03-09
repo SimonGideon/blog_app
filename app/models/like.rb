e1c7cc7a01e87d4d9e467dbe169bfc079ba20b75
SimonGideon <simongideon918@gmail.com>
2023-03-09 11:27:36 +0300

    Auto_update counter

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,4 +1,10 @@
 class Like < ApplicationRecord
   belongs_to :user
   belongs_to :post
+
+  after_save :update_likes_counter
+
+  def update_likes_counter
+    update(likes_counter: likes.count)
+  end
 end
