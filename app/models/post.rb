2369924f2fb13dbdcd76494c1433168cdcf9cdfb
SimonGideon <simongideon918@gmail.com>
2023-03-09 03:19:48 +0300

    Add methods

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,17 @@
 class Post < ApplicationRecord
   belongs_to :user
+  has_many :comments
+  has_many :likes
+
+  def recent_comments
+    comments.order(created_at: :desc).limit(5)
+  end
+
+  def update_comments_counter
+    update(comments_counter: comments.count)
+  end
+
+  def update_likes_counter
+    update(likes_counter: likes.count)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,10 @@
 class User < ApplicationRecord
+    has_many :posts
+    def recent_posts
+        posts.order(created_at: :desc).limit(3)
+    end
+
+    def update_posts_counter
+        update(posts_counter: posts.count)
+    end
 end
