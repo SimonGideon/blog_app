de50ff9871ec50eaa18ef28cfed5675b26726d0d
SimonGideon <simongideon918@gmail.com>
2023-03-10 18:55:08 +0300

    Refactor methods

--- app/models/post.rb
+++ app/models/post.rb
@@ -7,7 +7,7 @@ class Post < ApplicationRecord
   validates :title, length: { maximum: 250 }
   validates :comment_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
-  before_save :update_post_counter
+  after_save :update_post_counter
 
   def update_post_counter
     user.update(posts_counter: user.posts.count)
