8a95638efb637cfc6e8202c40251874bc6a3eb9a
SimonGideon <simongideon918@gmail.com>
2023-03-14 01:47:54 +0300

    Use resource in routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,7 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
+  resources :users, only: [:index, :show] do
+    resources :posts, only: [:index, :show]
+  end
   get '/', to: 'users#index'
-  get '/users', to: 'users#index'
-  get '/users/:id', to: 'users#show'
-  get '/users/:id/post', to: 'posts#index'
-  get '/users/:id/post/:id', to: 'posts#show'
 
 end
