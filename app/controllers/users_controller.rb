7c8b0ff281c2ef6c0feb7665ac45d26bd220f99d
SimonGideon <simongideon918@gmail.com>
2023-03-14 00:29:06 +0300

    Initial commit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,7 @@ class UsersController < ApplicationController
     end
 
     def show
-        @posts = Post.find(params[:id])
+        @user = User.find(params[:id])
+        # @posts = Post.find(params[:id])
     end
 end
