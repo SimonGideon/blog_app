db409527790d4dd94f78eccdade20f1c598b74b4
SimonGideon <simongideon918@gmail.com>
2023-03-14 01:05:57 +0300

    User-post Controllers

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,10 @@
+class PostsController < ApplicationController
+    def index
+        user = User.find(params[:id])
+        @posts = user.posts
+    end
+
+    def show
+        @posts = user.posts.find(params[:id])
+    end
+end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,5 @@ class UsersController < ApplicationController
 
     def show
         @user = User.find(params[:id])
-        # @posts = Post.find(params[:id])
     end
 end
