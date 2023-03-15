317458eee163ace4c1521541fcb17c566aa3c637
SimonGideon <simongideon918@gmail.com>
2023-03-15 10:40:12 +0300

    Refactor routers

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  resources :users, only: [:index, :show] do
-    resources :posts, only: [:index, :show]
-  end
-  get '/', to: 'users#index'
+  root 'users#index'
 
+resources :users, only: [:index, :show] do
+  resources :posts, only: [:index, :show]
 end
+end
+
+
