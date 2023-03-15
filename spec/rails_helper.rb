ff428bdeeb36d00177e434594d01fd3cb9b103ff
SimonGideon <simongideon918@gmail.com>
2023-03-15 12:27:11 +0300

    test for user controller

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -41,7 +41,7 @@ RSpec.configure do |config|
 
   # You can uncomment this line to turn off ActiveRecord support entirely.
   # config.use_active_record = false
-
+  config.include Rails.application.routes.url_helpers
   # RSpec Rails can automatically mix in different behaviours to your tests
   # based on their file location, for example enabling you to call `get` and
   # `post` in specs under `spec/controllers`.
--- spec/requests/users_request_spec.rb
+++ spec/requests/users_request_spec.rb
@@ -6,5 +6,17 @@ RSpec.describe 'Users', type: :request do
       get users_path
       expect(response).to have_http_status(200)
     end
+    it  'renders the index template' do
+      get users_path
+      expect(response).to render_template("index")
+    end
+
+    it 'body should contain the user name' do
+      @users = User.all
+      get users_path
+      @users.each do |user|
+        expect(response.body).to include(user.name)
+      end
+    end
   end
 end
