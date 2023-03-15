0c8c264a7b7a01f2374ac9823440fd15e81a4a14
SimonGideon <simongideon918@gmail.com>
2023-03-15 10:40:12 +0300

    Refactor routers

--- spec/requests/posts_request_spec.rb
+++ spec/requests/posts_request_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe 'Posts', type: :request do
-  # write your tests here
+  describe 'GET users/posts' do
+    it 'works! right status code given' do
+      get users_path
+      expect(response).to have_http_status(200)
+    end
+  end
 end
--- spec/requests/users_request_spec.rb
+++ spec/requests/users_request_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe 'Users', type: :request do
-  # write your tests here
+  describe 'GET /users' do
+    it 'works! right status code given' do
+      get users_path
+      expect(response).to have_http_status(200)
+    end
+  end
 end
