6ed8419796c0a53fc05e4154eb951adba77f7f33
SimonGideon <simongideon918@gmail.com>
2023-03-16 04:08:40 +0300

    User-views

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,8 +5,6 @@ class Post < ApplicationRecord
 
   validates :title, presence: true
   validates :title, length: { maximum: 250 }
-  validates :comment_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
-  validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   after_save :update_post_counter
 
   def update_post_counter
@@ -17,3 +15,5 @@ class Post < ApplicationRecord
     comments.order(created_at: :desc).limit(5)
   end
 end
+
+
