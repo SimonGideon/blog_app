1d6749e39702991ded81bf5b0873dd1a280f490a
SimonGideon <simongideon918@gmail.com>
2023-03-17 15:38:09 +0300

    Likes

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,7 +5,7 @@ class CommentsController < ApplicationController
       @comment.post = @post
       
       if @comment.save
-        redirect_to user_post_path(user_id: @post.user_id, id: @post.id), notice: "Comment created successfully."
+        redirect_to user_post_path(user_id: @post.user_id, id: @post.id)
       else
         render "posts/show"
       end
--- config/routes.rb
+++ config/routes.rb
@@ -5,11 +5,7 @@ Rails.application.routes.draw do
   resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :show, :new, :create] do
       resources :comments, only: [:create]
+      resources :likes, only: [:create, :destroy]
     end
   end
 end
-
-
-
-
-
