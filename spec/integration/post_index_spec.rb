a5062104a89b7368c08438523e89b31898fe04bb
Amen TETTEH <tkamen@outlook.com>
2023-03-21 23:48:25 +0000

    Update post_index_spec.rb file to add integration test

--- spec/integration/post_index_spec.rb
+++ spec/integration/post_index_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
-
-RSpec.describe 'Post index page', type: :feature do
-  #add tests here
+RSpec.describe 'User index page', type: :feature do
+  it 'displays usernames, profile pictures, and post count for each user' do
+    first_user = User.create(name: 'Ellon', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', posts_counter: 0,
+                             bio: 'Teacher from Nairobi.')
+    Post.create(user_id: first_user, title: 'Hello', text: 'This is my first post', comment_counter: 0,
+                likes_counter: 0)
+    visit '/users'
+    expect(page).to have_content(first_user.name)
+    expect(page).to have_css("img[src*='#{first_user.photo}']")
+    expect(page).to have_content(first_user.posts_counter)
+    click_on first_user.name
+    expect(current_path).to eq(user_path(first_user))
+  end
 end
