74452e3efa380014bd0e200360554593adee5bc6
SimonGideon <simongideon918@gmail.com>
2023-03-22 14:09:24 +0300

    Initial commit

--- spec/integration/user_index_spec.rb
+++ spec/integration/user_index_spec.rb
@@ -4,9 +4,8 @@ RSpec.describe 'User index page', type: :feature do
   it 'displays usernames, profile pictures, and post count for each user' do
     first_user = User.create(name: 'Dolph', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', posts_counter: 0,
                              bio: 'Teacher from Mexico.')
-    Post.create(user_id: first_user.id, title: 'Hello', text: 'This is my first post', comment_counter: 0,
+    post1 = Post.create(user_id: first_user.id, title: 'Hello', text: 'This is my first post', comment_counter: 0,
                 likes_counter: 0)
-
     visit '/users'
     expect(page).to have_content(first_user.name)
     expect(page).to have_css("img[src*='#{first_user.photo}']")
