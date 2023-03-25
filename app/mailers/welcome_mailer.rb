7c2a8e1a55c75dafd60b960a5bf4f44a49404580
SimonGideon <simongideon918@gmail.com>
2023-03-25 15:31:25 +0300

    Solve linters

--- app/mailers/welcome_mailer.rb
+++ app/mailers/welcome_mailer.rb
@@ -1,9 +1,8 @@
 class WelcomeMailer < ApplicationMailer
-
   def send_greetings_notification(user)
     @user = user
-    mail to: @user.email, subject: "Thank you for signing up!"
+    mail to: @user.email, subject: 'Thank you for signing up!'
 
-    mail to: "to@example.org"
+    mail to: 'to@example.org'
   end
 end
